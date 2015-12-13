970803c462b49e1025f5ae6ec2da6949e2b7eb53
bruceHome <1584764382@qq.com>
2015-12-13 10:15:57 +0800

    重构代码更新copyright

--- app/src/main/java/com/study/bruce/demo/DemoApplication.java
+++ app/src/main/java/com/study/bruce/demo/DemoApplication.java
@@ -1,12 +1,9 @@
-
 /*
  * Copyright (c) 2015.
- *   This document is just for Bruce's personal study.
- *   Some resources come from the Internet. Everyone can download and use it for study, but can
- *   not be used for commercial purpose. The author does not bear the
- *   corresponding disputes arising therefrom.
- *   Please delete within 24 hours after download.
- *   If you have good suggestions for this code, you can contact BurrceHurrican@foxmail.com.
+ *   This document is Bruce's individual learning the android demo, wherein the use of the code from the Internet, only to use as a learning exchanges.
+ *   And where any person can download and use, but not for commercial purposes.
+ *   Author does not assume the resulting corresponding disputes.
+ *   If you have good suggestions for the code, you can contact BurrceHurrican@foxmail.com
  *   本文件为Bruce's个人学习android的demo, 其中所用到的代码来源于互联网，仅作为学习交流使用。
  *   任和何人可以下载并使用, 但是不能用于商业用途。
  *   作者不承担由此带来的相应纠纷。
@@ -30,7 +27,7 @@ import java.util.List;
  * 全局 application
  * Created by BruceHurrican on 2015/6/7.
  */
-public class DemoApplication extends Application{
+public class DemoApplication extends Application {
     // used in volley_demo
     private static RequestQueue queues;
     private List<Activity> container;
